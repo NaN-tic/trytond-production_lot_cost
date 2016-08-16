d4ee2050df54158f542c036dae9c900a771da94a
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-08-16 10:27:10 +0200

    Upgrade test

--- tests/test_production_lot_cost.py
+++ tests/test_production_lot_cost.py
@@ -4,23 +4,14 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_depends, test_view
+from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('production_lot_cost')
-
-    def test0005views(self):
-        'Test views'
-        test_view('production')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'production_lot_cost'
 
 
 def suite():
@@ -28,9 +19,11 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
     suite.addTests(doctest.DocFileSuite('scenario_production_lot_cost.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     suite.addTests(doctest.DocFileSuite(
             'scenario_production_lot_cost_with_output_lot.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
